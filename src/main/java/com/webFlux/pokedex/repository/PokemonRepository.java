4a89e3cd68b04fc1965fe73ecaa4bfac4e52ba21
Bruno Henrique Félix Dórea <brunodorea@outlook.com.br>
2022-09-13 10:39:24 -0300

    Initial commit

---
+++ src/main/java/com/webFlux/pokedex/repository/PokemonRepository.java
@@ -0,0 +1,7 @@
+package com.webFlux.pokedex.repository;
+
+import com.webFlux.pokedex.model.Pokemon;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface PokemonRepository extends ReactiveMongoRepository<Pokemon, String> {
+}
